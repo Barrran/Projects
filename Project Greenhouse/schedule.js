f2f1ca3eb90e0bb0b032f8f31928df6e248df6e7
barrran <anthonybarran@gmail.com>
2024-05-11 17:38:37 -0400

    lights and fan logic

--- Project Greenhouse/schedule.js
+++ Project Greenhouse/schedule.js
@@ -8,33 +8,41 @@
 //test logic with javascript then convert to arduino for real world application testing.
 
 //Global Variables
+
 let time = 0;
 let fan = 0;
 let light = 0;
 let heater = 0;
 let temperature = 0.0;
-
-//Time measurement function
-
+const startDate = new Date;
+const lowTemp = 20;
+const hiTemp = 24;
 
 //Fan operation function
 
-function turnOnFan() {
-    fan = 1;
-    return fan;
-};
-
-function turnOffFan() {
-    fan = 0;
-    return fan;
-};
-
 function controlFan() {
-    turnOnFan();
-    const duration = 15 * 60 * 1000;
-    setTimeout(()=>{turnOffFan();}, duration);
-};
 
+    function turnOnFan() {
+        fan = 1;
+        return fan;
+    };
+    
+    function turnOffFan() {
+        fan = 0;
+        return fan;
+    };
+
+    turnOnFan();
+
+    setTimeout( () => {
+        turnOffFan();
+        setInterval( () => {
+            turnOnFan();
+            setTimeout(turnOffFan, 15 * 60 * 1000);
+        }, 45 * 60 * 1000);
+    }, 15 * 60 * 1000);
+    
+    };
 
 //UV light operation function
 
@@ -75,13 +83,40 @@ function controlHeater() {
 //Control flow logic goes here:
 
 function runGreenHouse () {
-    const startDate = new Date;
-    const startTime = getTime();
+
     console.log(`The current date is ${startDate}`);
     console.log("Default settings are as follows:");
     console.log("1. Lights on for 18 hours and off for 6 hours.");
     console.log("2. Fan on for 15mins every 60mins.");
     console.log("3. Heater on at 20 DegC and off at 24 DegC.");
+
+//Calculate time to turn on and off light.
+
+    const lightOn = new Date(startDate.getTime() + 60 * 60 * 1000);
+    const lightOff = new Date(startDate.getTime() + 18 * 60 * 60 * 1000);
+
+ //Display times to turn on and off.
+
+    console.log(`Lights will turn on ${lightOn}`);
+    console.log(`Lights will turn off ${lightOff}`);
+
+ //Calculate time to turn on and off fan.   
+
+    const fanOn = new Date(startDate.getTime() + 10 * 60 * 1000);
+
+    console.log(`The fan will turn on ${fanOn} and run for 15 minutes.`);
+
+ //Calculate heater on and off times, manage temperature.
+ 
+    if (lowTemp) {
+        turnOnHeat();
+        for(i=0, i>hiTemp, i++);
+        
+    } else {
+        turnOffHeat();
+    }
+
+
 };
 
 runGreenHouse();
