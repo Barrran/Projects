819e3bf451611b1e09053cc8ec145b6d38ecf6b8
Barrran <154044864+Barrran@users.noreply.github.com>
2025-03-03 09:48:40 -0500

    Create node.js
    backend code for website 03/03/2025

---
+++ HVAC Site/Html Files/node.js
@@ -0,0 +1,19 @@
+const stripe = require('stripe')('YOUR_STRIPE_SECRET_KEY');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+    try {
+        const { amount, currency } = req.body;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency,
+        });
+        res.send({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
